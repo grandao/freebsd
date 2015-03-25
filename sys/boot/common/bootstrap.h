cb5e05f96b54caafd92004051bd3cad297d039d0
rpaulo <rpaulo@FreeBSD.org>
2015-03-25 06:40:19 +0000

    interp_parse: make the parse string a const argument.

--- sys/boot/common/bootstrap.h
+++ sys/boot/common/bootstrap.h
@@ -33,11 +33,30 @@
 #include <sys/queue.h>
 #include <sys/linker_set.h>
 
+/*
+ * Generic device specifier; architecture-dependant
+ * versions may be larger, but should be allowed to
+ * overlap.
+ */
+struct devdesc
+{
+    struct devsw	*d_dev;
+    int			d_type;
+#define DEVT_NONE	0
+#define DEVT_DISK	1
+#define DEVT_NET	2
+#define	DEVT_CD		3
+#define DEVT_ZFS	4
+    int			d_unit;
+    void		*d_opendata;
+};
+
 /* Commands and return values; nonzero return sets command_errmsg != NULL */
 typedef int	(bootblk_cmd_t)(int argc, char *argv[]);
 #define	COMMAND_ERRBUFSZ	(256)
 extern char	*command_errmsg;	
 extern char	command_errbuf[COMMAND_ERRBUFSZ];
+
 #define CMD_OK		0
 #define CMD_WARN	1
 #define CMD_ERROR	2
@@ -52,7 +71,7 @@ int	include(const char *filename);
 char	*backslash(char *str);
 
 /* interp_parse.c */
-int	parse(int *argc, char ***argv, char *str);
+int	parse(int *argc, char ***argv, const char *str);
 
 /* interp_forth.c */
 void	bf_init(const char *rc);
@@ -94,7 +113,7 @@ struct bcache_devdata
 /*
  * Modular console support.
  */
-struct console 
+struct console
 {
     const char	*c_name;
     const char	*c_desc;
@@ -116,7 +135,7 @@ void		cons_probe(void);
 /*
  * Plug-and-play enumerator/configurator interface.
  */
-struct pnphandler 
+struct pnphandler
 {
     const char	*pp_name;		/* handler/bus name */
     void	(* pp_enumerate)(void);	/* enumerate PnP devices, add to chain */
@@ -163,7 +182,7 @@ extern int			isapnp_readport;
  * Metadata are allocated on our heap, and copied into kernel space
  * before executing the kernel.
  */
-struct file_metadata 
+struct file_metadata
 {
     size_t			md_size;
     u_int16_t			md_type;
@@ -253,9 +272,9 @@ int __elfN(load_modmetadata)(struct preloaded_file *fp, u_int64_t dest);
 #endif
 
 /*
- * Support for commands 
+ * Support for commands
  */
-struct bootblk_command 
+struct bootblk_command
 {
     const char		*c_name;
     const char		*c_desc;
@@ -269,7 +288,7 @@ struct bootblk_command
 
 SET_DECLARE(Xcommand_set, struct bootblk_command);
 
-/* 
+/*
  * The intention of the architecture switch is to provide a convenient
  * encapsulation of the interface between the bootstrap MI and MD code.
  * MD code may selectively populate the switch at runtime based on the
